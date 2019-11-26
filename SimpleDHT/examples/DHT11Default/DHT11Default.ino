fdccb5068ef5b818baf0098d3597b77e2f68a61d
sCHROCK000 <schrock000@gmail.com>
2019-11-26 13:53:50 +0800

    modify an example

--- SimpleDHT/examples/DHT11Default/DHT11Default.ino
+++ SimpleDHT/examples/DHT11Default/DHT11Default.ino
@@ -1,10 +1,6 @@
 #include <SimpleDHT.h>
 
-// for DHT11, 
-//      VCC: 5V or 3V
-//      GND: GND
-//      DATA: 2
-int pinDHT11 = 2;
+int pinDHT11 = 23;
 SimpleDHT11 dht11;
 
 void setup() {
@@ -12,11 +8,8 @@ void setup() {
 }
 
 void loop() {
-  // start working...
   Serial.println("=================================");
-  Serial.println("Sample DHT11...");
   
-  // read without samples.
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
@@ -29,6 +22,5 @@ void loop() {
   Serial.print((int)temperature); Serial.print(" *C, "); 
   Serial.print((int)humidity); Serial.println(" H");
   
-  // DHT11 sampling rate is 1HZ.
   delay(1500);
 }
